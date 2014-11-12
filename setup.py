c627eef054644bf1d95b9bb202c6ebc5804eb373
Narahari Allamraju <anarahari@gmail.com>
2014-11-12 19:48:36 +0800

    tweaked setup.py

--- setup.py
+++ setup.py
@@ -3,12 +3,17 @@
 from distutils.core import setup
 
 setup(name='pyzmq-wrapper',
-      version='0.2',
-      description='Wrapper classes for pyzmq',
-      long_description='Please visit https://github.com/supercoderz/pyzmq-wrapper for more details',
-      author='Narahari Allamraju',
-      author_email='anarahari@gmail.com',
-      url='https://github.com/supercoderz/pyzmq-wrapper',
-      packages=['zmqwrapper'],
-      requires=['pyzmq','pytest'],
-     )
+	  version='0.2',
+	  description='Wrapper classes for pyzmq',
+	  long_description='Please visit https://github.com/supercoderz/pyzmq-wrapper for more details',
+	  author='Narahari Allamraju',
+	  author_email='anarahari@gmail.com',
+	  url='https://github.com/supercoderz/pyzmq-wrapper',
+	  packages=['zmqwrapper'],
+	  requires=['pyzmq','pytest'],
+	classifiers = [
+		'Programming Language :: Python :: 2.7',
+		'Programming Language :: Python :: 3.4',
+		'Programming Language :: Python :: 3',
+	]
+	 )
