4e57e00683d102961445448d7457901b16557c80
Hari <anarahari@gmail.com>
2016-08-15 15:30:28 +0900

    fixed install requires

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@
 from distutils.core import setup
 
 setup(name='pyzmq-wrapper',
-	  version='0.2',
+	  version='0.2.1',
 	  description='Wrapper classes for pyzmq',
 	  long_description='Please visit https://github.com/supercoderz/pyzmq-wrapper for more details',
 	  author='Narahari Allamraju',
 	  author_email='anarahari@gmail.com',
 	  url='https://github.com/supercoderz/pyzmq-wrapper',
 	  packages=['zmqwrapper'],
-	  requires=['pyzmq','pytest'],
+	  install_requires=['pyzmq','pytest'],
 	classifiers = [
 		'Programming Language :: Python :: 2.7',
 		'Programming Language :: Python :: 3.4',
